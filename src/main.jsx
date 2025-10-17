cae21ae5f0da3a0ce35f4e31bc31325de84d10a7
JoelML1 <joesmele3@gmail.com>
2025-10-17 08:36:39 -0500

    deploy: trigger github pages build

--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,4 @@ import './index.css' // <- Asegúrate de tener esta línea
 import './index.css' // (deja solo una si ya existía)
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 //33213
+//a
