afacdc529a5beb41bcfb349e5badc5a89a655a94
JoelML1 <joesmele3@gmail.com>
2025-10-17 08:35:41 -0500

    a

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,4 @@ import App from './App.jsx'
 import './index.css' // <- Asegúrate de tener esta línea
 import './index.css' // (deja solo una si ya existía)
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+//33213
